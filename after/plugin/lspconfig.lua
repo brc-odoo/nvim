b7659b434823688c5b3e84370df9d89b78db22c1
Brian <brc@odoo.com>
2024-09-28 21:15:43 -0700

    [add] pylsp ignores

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -5,41 +5,35 @@ capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
 local servers = { 'pylsp', 'eslint' }
 
 lspconfig.pylsp.setup{
-	settings = {
-		pylsp = {
-			plugins = {
-				pycodestyle = { enabled = false },
-				flake8 = {
-					enabled = true,
-					ignore = {
-						'W391',
-						'E301',
-						'E302',
-						'E501',
-						'W504'
-					},
-					maxLineLength = 200
-				},
-				pylint = {
-					enabled = false,
-					executable = 'pylint'
-				},
-				jedi_completion = { fuzzy = true },
-				isort = { enabled = true },
-			}
-		}
-	},
-	flags = {
-		debounce_text_changes = 200,
-	},
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = { enabled = false },
+                flake8 = {
+                    enabled = true,
+                    ignore = 'W391, E301, E302, E501, W504',
+                    maxLineLength = 200
+                },
+                pylint = {
+                    enabled = false,
+                    executable = 'pylint'
+                },
+                jedi_completion = { fuzzy = true },
+                isort = { enabled = true },
+            }
+        }
+    },
+    flags = {
+        debounce_text_changes = 200,
+    },
 }
 lspconfig.eslint.setup({
-	on_attach = function(client, bufnr)
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			buffer = bufnr,
-			command = "EslintFixAll",
-		})
-	end,
+    on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "EslintFixAll",
+        })
+    end,
 })
 
 for _, lsp in ipairs(servers) do
