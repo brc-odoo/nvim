2fe1bee68711c335973eef65b4bc8acbe40fba58
Brian <brc@odoo.com>
2025-03-06 10:37:19 -0800

    [FIX] docker_ls fixed to dockerls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,7 +10,7 @@ return {
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = { "pylsp", "denols", "lemminx", "eslint", "lua_ls", "docker_ls", "ts_ls", "tailwindcss" }
+            ensure_installed = { "pylsp", "denols", "lemminx", "eslint", "lua_ls", "dockerls", "ts_ls", "tailwindcss" }
         })
     end,
 }
